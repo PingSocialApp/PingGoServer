ccb73eba6e30922b317c6a47cce957af7a6127a5
Sparsh Patel <sparshypatel@gmail.com>
2021-03-30 17:57:15 -0700

    Update users.handler.go

--- src/handlers/users.handler.go
+++ src/handlers/users.handler.go
@@ -14,9 +14,9 @@ func GetUserBasic(c *gin.Context) {
 	//TODO add rest of query
 	transaction, err := data.WriteTransaction(func(transaction neo4j.Transaction) (interface {}, error){
 		result, err := transaction.Run(
-			"MATCH (userA:User {user_id: $uid}) RETURN userA.name",
+			"MATCH (userA:User {user_id: $uid}) RETURN userA.name, userA.bio, userA.profilepic",
 			map[string]interface{}{
-				"uid": c.Param("uid"),
+				"uid": "users/" + c.Param("uid"),
 			})
 		if err != nil {
 			return nil, err
@@ -32,16 +32,71 @@ func GetUserBasic(c *gin.Context) {
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
+			"isEmpty": true,
+			"data": nil,
 		})
 	}else{
 		c.JSON(http.StatusOK, gin.H{
-			"name": transaction,
+			"error": nil,
+			"isEmpty": false,
+			"data": gin.H{
+				"name": transaction.([]interface{})[0],
+				"bio": transaction.([]interface{})[1],
+				"profilepic": transaction.([]interface{})[2],
+			},
 		})
 	}
 }
 
 func GetUserSocials(c *gin.Context) {
+	data := dbclient.CreateSession()
+	defer data.Close()
 
+	//TODO add rest of query
+	transaction, err := data.WriteTransaction(func(transaction neo4j.Transaction) (interface {}, error){
+		result, err := transaction.Run(
+			"MATCH (userA:User {user_id: $user_id})-[:DIGITAL_PROFILE]->(social:Socials)\n" +
+				"RETURN social.number, social.perEmail, social.ig, social.sc, social.fb, social.tt, " +
+				"social.tw, social.venmo, social.proEmail, social.li, social.website",
+			map[string]interface{}{
+				"uid": "users/" + c.Param("uid"),
+			})
+		if err != nil {
+			return nil, err
+		}
+
+		if result.Next() {
+			return result.Record().Values(), nil
+		}
+
+		return nil, result.Err()
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+			"isEmpty": true,
+			"data": nil,
+		})
+	}else{
+		c.JSON(http.StatusOK, gin.H{
+			"error": nil,
+			"isEmpty": false,
+			"data": gin.H{
+				"number": transaction.([]interface{})[0],
+				"perEmail": transaction.([]interface{})[1],
+				"ig": transaction.([]interface{})[2],
+				"sc": transaction.([]interface{})[3],
+				"fb": transaction.([]interface{})[4],
+				"tt": transaction.([]interface{})[5],
+				"tw": transaction.([]interface{})[6],
+				"venmo": transaction.([]interface{})[7],
+				"proEmail": transaction.([]interface{})[8],
+				"li": transaction.([]interface{})[9],
+				"website": transaction.([]interface{})[10],
+			},
+		})
+	}
 }
 
 func CheckoutUser(c *gin.Context) {
@@ -105,7 +160,45 @@ func CreateNewUser(c *gin.Context) {
 }
 
 func UpdateUserInfo(c *gin.Context) {
+	data := dbclient.CreateSession()
+	defer data.Close()
 
+	//TODO add rest of query
+	transaction, err := data.WriteTransaction(func(transaction neo4j.Transaction) (interface {}, error){
+		result, err := transaction.Run(
+			"MATCH (userA:User {user_id: @}) \nSET userA.user_id=@, userA.name =@,  userA.bio=@," +
+				" userA.profilepic=@, userA.sex=@, userA.notifToken=@, userA.userType=@, userA.rating=@ ",
+			map[string]interface{}{
+				"uid": "users/" + c.Param("uid"),
+			})
+		if err != nil {
+			return nil, err
+		}
+
+		if result.Next() {
+			return result.Record().Values(), nil
+		}
+
+		return nil, result.Err()
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+			"isEmpty": true,
+			"data": nil,
+		})
+	}else{
+		c.JSON(http.StatusOK, gin.H{
+			"error": nil,
+			"isEmpty": false,
+			"data": gin.H{
+				"name": transaction.([]interface{})[0],
+				"bio": transaction.([]interface{})[1],
+				"profilepic": transaction.([]interface{})[2],
+			},
+		})
+	}
 }
 
 func SetNotifToken(c *gin.Context) {
