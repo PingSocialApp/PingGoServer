29ece4235cec6e7f7c9f9b56c9a4711d4105ebbb
Sree Grandhe <sreegrandhe@live.com>
2021-06-14 10:17:02 -0500

    WIP Implemented Authorization

--- go-code/src/go.mod
+++ go-code/src/go.mod
@@ -6,7 +6,7 @@ require (
 	cloud.google.com/go/firestore v1.4.0
 	cloud.google.com/go/storage v1.12.0 // indirect
 	firebase.google.com/go v3.13.0+incompatible
-	github.com/dustin/go-broadcast v0.0.0-20171205050544-f664265f5a66
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
 	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/neo4j/neo4j-go-driver v1.8.3
--- go-code/src/handlers/links.handler.go
+++ go-code/src/handlers/links.handler.go
@@ -2,25 +2,36 @@ package handlers
 
 import (
 	"encoding/json"
-	"github.com/gin-gonic/gin"
-	"github.com/neo4j/neo4j-go-driver/neo4j"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	dbclient "pingserver/db_client"
 	firebase "pingserver/firebase_client"
 	"strconv"
 	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/neo4j/neo4j-go-driver/neo4j"
 )
 
 //Requests
 func DeleteRequest(c *gin.Context) {
+	uid, exists := c.Get("uid")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "ID not set from Authentication",
+			"data":  nil,
+		})
+	}
+
 	session := dbclient.CreateSession()
 	defer dbclient.KillSession(session)
 
 	_, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
 		_, err := transaction.Run(
-			"MATCH (:User)-[link:REQUESTED {link_id:$lid}]->(:User)\nDELETE link;",
+			"MATCH (:User {uid: $uid})-[link:REQUESTED {link_id:$lid}]->(:User)\n DELETE link;",
 			gin.H{
+				"uid": uid,
 				"lid": c.Param("rid"),
 			},
 		)
@@ -32,13 +43,13 @@ func DeleteRequest(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 	}
 	c.JSON(http.StatusOK, gin.H{
-		"error":   nil,
-		"data":    "Request Deleted",
+		"error": nil,
+		"data":  "Request Deleted",
 	})
 
 }
@@ -49,7 +60,7 @@ func AcceptRequest(c *gin.Context) {
 
 	_, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
 		_, err := transaction.Run(
-			"MATCH (userA:User)-[request:REQUESTED {link_id: $link_id}]->(userB:User)"+
+			"MATCH (userA:User {})-[request:REQUESTED {link_id: $link_id}]->(userB:User)"+
 				"MERGE (userA:User)-[link:LINKED {link_id: request.link_id, permissions: request.permissions}]->(userB:User)"+
 				"DELETE request",
 			gin.H{
@@ -64,16 +75,14 @@ func AcceptRequest(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
-		"error":   nil,
-		"isEmpty": false,
-		"data":    "Request Accepted",
+		"error": nil,
+		"data":  "Request Accepted",
 	})
 }
 
@@ -82,25 +91,28 @@ func DeclineRequest(c *gin.Context) {
 }
 
 func SendRequest(c *gin.Context) {
-	session := dbclient.CreateSession()
-	defer dbclient.KillSession(session)
+	uid, exists := c.Get("uid")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "ID not set from Authentication",
+			"data":  nil,
+		})
+	}
 
 	var jsonData gin.H // map[string]interface{}
 	data, _ := ioutil.ReadAll(c.Request.Body)
 	if e := json.Unmarshal(data, &jsonData); e != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   e.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": e.Error(),
+			"data":  nil,
 		})
 		return
 	}
 
-	var permissions int64 = 0
-	for i := 0; i < len(jsonData["permissions"].([]interface{})); i++ {
-		permissions |= jsonData["permissions"].([]interface{})[i].(int64) << (len(jsonData["permissions"].([]interface{})) - i)
-	}
-	jsonData["permissions"] = permissions
+	jsonData["userA_id"] = uid
+
+	session := dbclient.CreateSession()
+	defer dbclient.KillSession(session)
 
 	output, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
 		exists, err := transaction.Run(
@@ -136,23 +148,20 @@ func SendRequest(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 	}
 
 	switch output {
 	case "exists":
 		c.JSON(http.StatusConflict, gin.H{
-			"error":   nil,
-			"isEmpty": false,
-			"data":    "Record already exists",
+			"error": nil,
+			"data":  "Record already exists",
 		})
 	case "created":
 		c.JSON(http.StatusOK, gin.H{
-			"error":   nil,
-			"isEmpty": false,
+			"error": nil,
 			"data": gin.H{
 				"id": jsonData["lid"],
 			},
@@ -161,24 +170,30 @@ func SendRequest(c *gin.Context) {
 }
 
 func GetOpenSentRequests(c *gin.Context) {
+	uid, exists := c.Get("uid")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "ID not set from Authentication",
+			"data":  nil,
+		})
+	}
+
 	session := dbclient.CreateSession()
 	defer dbclient.KillSession(session)
 
 	offset, err := strconv.Atoi(c.Query("offset"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 	limit, err := strconv.Atoi(c.Query("limit"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
@@ -188,7 +203,7 @@ func GetOpenSentRequests(c *gin.Context) {
 			"MATCH (userA:User)-[link:REQUESTED]->(userB:User {user_id: $user_id})\nRETURN userA.id AS id, "+
 				"userA.name AS name, userA.bio AS bio, userA.profilepic AS profilepic link.link_id AS linkId SKIP $offset LIMIT $limit;",
 			gin.H{
-				"user_id": c.Query("uid"),
+				"user_id": uid,
 				"offset":  offset,
 				"limit":   limit,
 			},
@@ -196,9 +211,10 @@ func GetOpenSentRequests(c *gin.Context) {
 		if err != nil {
 			return nil, err
 		}
-		var records []interface{}
+		records := make([]interface{}, 0)
 		for data.Next() {
 			records = append(records, gin.H{
+				"id":         ValueExtractor(data.Record().Get("id")).(string),
 				"name":       ValueExtractor(data.Record().Get("name")).(string),
 				"bio":        ValueExtractor(data.Record().Get("bio")).(string),
 				"profilepic": ValueExtractor(data.Record().Get("profilepic")).(string),
@@ -210,40 +226,45 @@ func GetOpenSentRequests(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"error":   nil,
-		"data":    data.([]interface{}),
+		"error": nil,
+		"data":  data,
 	})
 }
 
 func GetOpenReceivedRequests(c *gin.Context) {
-	session := dbclient.CreateSession()
-	defer dbclient.KillSession(session)
-
 	offset, err := strconv.Atoi(c.Query("offset"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 	limit, err := strconv.Atoi(c.Query("limit"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
+	uid, exists := c.Get("uid")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "ID not set from Authentication",
+			"data":  nil,
+		})
+	}
+
+	session := dbclient.CreateSession()
+	defer dbclient.KillSession(session)
 
 	data, err := session.ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
 		data, err := transaction.Run(
@@ -251,7 +272,7 @@ func GetOpenReceivedRequests(c *gin.Context) {
 				"RETURN userA.id AS id, userA.name AS name, userA.bio AS bio, userA.profilepic AS profilepic, link.link_id AS linkId "+
 				"SKIP $offset LIMIT $limit;",
 			gin.H{
-				"user_id": c.Query("uid"),
+				"user_id": uid,
 				"offset":  offset,
 				"limit":   limit,
 			},
@@ -259,9 +280,10 @@ func GetOpenReceivedRequests(c *gin.Context) {
 		if err != nil {
 			return nil, err
 		}
-		var records []interface{}
+		records := make([]interface{}, 0)
 		for data.Next() {
 			records = append(records, gin.H{
+				"id":         ValueExtractor(data.Record().Get("id")).(string),
 				"name":       ValueExtractor(data.Record().Get("name")).(string),
 				"bio":        ValueExtractor(data.Record().Get("bio")).(string),
 				"profilepic": ValueExtractor(data.Record().Get("profilepic")).(string),
@@ -273,57 +295,68 @@ func GetOpenReceivedRequests(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"error":   nil,
-		"isEmpty": len(data.([]interface{})) == 0,
-		"data":    data.([]interface{}),
+		"error": nil,
+		"data":  data.([]interface{}),
 	})
 }
 
 //Links
 func GetFromSocials(c *gin.Context) {
-
-	permissions, err := getPermissions(c.Param("id"),c.Query("myUID"))
-	if err != nil{
+	uid, exists := c.Get("uid")
+	if !exists {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": "ID not set from Authentication",
+			"data":  nil,
 		})
-		return
+	}
+
+	permissions, err := getPermissions(c.Param("id"), uid.(string))
+	if err != nil {
+		if err.Error() != "No Link Found" {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+				"data":  nil,
+			})
+			return
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"error": nil,
+				"data":  nil,
+			})
+			return
+		}
 	}
 
 	get, err := firebase.Firestore.Collection("socials").Doc(c.Param("id")).Get(c)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 
 	socialMedia := get.Data()
 	socials := gin.H{
-		"instagram": nil,
-		"snapchat": nil,
-		"facebook": nil,
-		"twitter": nil,
-		"linkedin": nil,
+		"instagram":         nil,
+		"snapchat":          nil,
+		"facebook":          nil,
+		"twitter":           nil,
+		"linkedin":          nil,
 		"professionalEmail": nil,
-		"personalEmail": nil,
-		"tiktok": nil,
-		"venmo": nil,
-		"website": nil,
-		"phone":nil,
-		"location":permissions[0],
+		"personalEmail":     nil,
+		"tiktok":            nil,
+		"venmo":             nil,
+		"website":           nil,
+		"phone":             nil,
+		"location":          permissions[0],
 	}
 	if permissions[11] {
 		socials["website"] = socialMedia["website"]
@@ -361,27 +394,43 @@ func GetFromSocials(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"error": nil,
-		"isEmpty": false,
-		"data": socials,
+		"data":  socials,
 	})
 }
 
 func GetToSocials(c *gin.Context) {
-	permissions, err := getPermissions(c.Query("myUID"),c.Param("id"))
-	if err != nil{
+	uid, exists := c.Get("UID")
+	if !exists {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"data":    nil,
+			"error": "Auth mis-match",
+			"data":  nil,
 		})
 		return
 	}
+
+	permissions, err := getPermissions(uid.(string), c.Param("id"))
+	if err != nil {
+		if err.Error() != "No Link Found" {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+				"data":  nil,
+			})
+			return
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"error": nil,
+				"data":  -1,
+			})
+			return
+		}
+	}
 	c.JSON(http.StatusOK, gin.H{
-		"error":   nil,
-		"data":    permissions,
+		"error": nil,
+		"data":  permissions,
 	})
 }
 
-func getPermissions(uidA string, uidB string) (permissions [12]bool, e error){
+func getPermissions(uidA string, uidB string) (permissions [12]bool, e error) {
 	session := dbclient.CreateSession()
 	defer dbclient.KillSession(session)
 
@@ -397,14 +446,20 @@ func getPermissions(uidA string, uidB string) (permissions [12]bool, e error){
 			return nil, err
 		}
 
-		return ValueExtractor(record.Record().Get("link.permissions")), nil
+		if record.Next() {
+			return ValueExtractor(record.Record().Get("link.permissions")), nil
+		} else if record.Err() != nil {
+			return nil, record.Err()
+		} else {
+			return nil, errors.New("No link found")
+		}
 	})
 
 	if err != nil {
 		return permissions, err
 	}
 
-	permissionsString := strconv.FormatInt(output.(int64),2)
+	permissionsString := strconv.FormatInt(output.(int64), 2)
 	for len(permissionsString) < 12 {
 		permissionsString = "0" + permissionsString
 	}
@@ -417,37 +472,39 @@ func getPermissions(uidA string, uidB string) (permissions [12]bool, e error){
 }
 
 func GetAllLinks(c *gin.Context) {
-	session := dbclient.CreateSession()
-	defer dbclient.KillSession(session)
+	uid, exists := c.Get("uid")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "ID not set from Authentication",
+			"data":  nil,
+		})
+	}
 
 	offset, err := strconv.Atoi(c.Query("offset"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 	limit, err := strconv.Atoi(c.Query("limit"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 
+	session := dbclient.CreateSession()
+	defer dbclient.KillSession(session)
+
 	data, err := session.ReadTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
 		data, err := transaction.Run(
-			"CALL {MATCH (userA:User {user_id: $user_id})-[:LINKED]->(userB:User)" +
-				"RETURN userB.name AS name, userB.user_id AS id, userB.profilepic AS profilepic, userB.bio AS bio" +
-				"UNION MATCH (userA:User)-[:LINKED]->(userB:User {user_id: $user_id})    " +
-				"RETURN userA.name AS name, userA.user_id AS id, userA.profilepic AS profilepic, userA.bio AS bio}" +
-				"RETURN name, id, profilepic, bio ORDER BY name SKIP $offset LIMIT $limit",
+			"CALL {MATCH (userA:User {user_id: $user_id})-[:LINKED]->(userB:User) RETURN userB.name AS name, userB.user_id AS id, userB.profilepic AS profilepic, userB.bio AS bio UNION MATCH (userA:User)-[:LINKED]->(userB:User {user_id: $user_id}) RETURN userA.name AS name, userA.user_id AS id, userA.profilepic AS profilepic, userA.bio AS bio} RETURN name, id, profilepic, bio ORDER BY name SKIP $offset LIMIT $limit",
 			gin.H{
-				"user_id": c.Query("uid"),
+				"user_id": uid,
 				"offset":  offset,
 				"limit":   limit,
 			},
@@ -455,13 +512,13 @@ func GetAllLinks(c *gin.Context) {
 		if err != nil {
 			return nil, err
 		}
-		var records []interface{}
+		records := make([]interface{}, 0)
 		for data.Next() {
 			records = append(records, gin.H{
 				"name":       ValueExtractor(data.Record().Get("name")).(string),
 				"bio":        ValueExtractor(data.Record().Get("bio")).(string),
 				"profilepic": ValueExtractor(data.Record().Get("profilepic")).(string),
-				"id":    ValueExtractor(data.Record().Get("id")).(string),
+				"id":         ValueExtractor(data.Record().Get("id")).(string),
 			})
 		}
 		return records, data.Err()
@@ -469,49 +526,54 @@ func GetAllLinks(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"error":   nil,
-		"isEmpty": len(data.([]interface{})) == 0,
-		"data":    data.([]interface{}),
+		"error": nil,
+		"data":  data,
 	})
 }
 
 func GetLastCheckedInLocations(c *gin.Context) {
-	session := dbclient.CreateSession()
-	defer dbclient.KillSession(session)
+	uid, exists := c.Get("uid")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "ID not set from Authentication",
+			"data":  nil,
+		})
+	}
 
 	offset, err := strconv.Atoi(c.Query("offset"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 	limit, err := strconv.Atoi(c.Query("limit"))
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   err.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 
+	session := dbclient.CreateSession()
+	defer dbclient.KillSession(session)
+
 	data, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
 		data, err := transaction.Run(
-			"MATCH (userB:User {isCheckedIn:true})-[link:LINKED]->(userA:User {user_id: $user_id})\nWHERE link.permissions >= 2048\n" +
-				"MATCH (userB)-[a:ATTENDING]->(e:Events)\nRETURN userB.name AS name, userB.user_id AS id, userB.profilepic AS profilepic, e.name AS eventName, e.event_id AS eventId, e.type AS eventType ORDER BY DESC a.timeAttended SKIP $offset LIMIT $limit",
+			"MATCH (userB:User {isCheckedIn:true})-[link:LINKED]->(userA:User {user_id: $user_id})\nWHERE link.permissions >= 2048\n"+
+				"MATCH (userB)-[a:ATTENDING]->(e:Events)\nRETURN userB.name AS name, userB.user_id AS id, userB.profilepic AS profilepic,\n"+
+				"e.name AS eventName, e.event_id AS eventId, e.type AS eventType ORDER BY DESC a.timeAttended SKIP $offset LIMIT $limit",
 			gin.H{
-				"user_id": c.Query("uid"),
+				"user_id": uid,
 				"offset":  offset,
 				"limit":   limit,
 			},
@@ -519,15 +581,15 @@ func GetLastCheckedInLocations(c *gin.Context) {
 		if err != nil {
 			return nil, err
 		}
-		var records []interface{}
+		records := make([]interface{}, 0)
 		for data.Next() {
 			records = append(records, gin.H{
 				"name":       ValueExtractor(data.Record().Get("name")).(string),
-				"id":        ValueExtractor(data.Record().Get("id")).(string),
+				"id":         ValueExtractor(data.Record().Get("id")).(string),
 				"profilepic": ValueExtractor(data.Record().Get("profilepic")).(string),
-				"eventName":    ValueExtractor(data.Record().Get("eventName")).(string),
+				"eventName":  ValueExtractor(data.Record().Get("eventName")).(string),
 				"eventId":    ValueExtractor(data.Record().Get("eventId")).(string),
-				"eventType":    ValueExtractor(data.Record().Get("eventType")).(string),
+				"eventType":  ValueExtractor(data.Record().Get("eventType")).(string),
 			})
 		}
 		return records, data.Err()
@@ -535,19 +597,27 @@ func GetLastCheckedInLocations(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"error":   nil,
-		"data":    data.([]interface{}),
+		"error": nil,
+		"data":  data,
 	})
 }
 
 func UpdatePermissions(c *gin.Context) {
+	uid, exists := c.Get("uid")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "ID not set from Authentication",
+			"data":  nil,
+		})
+	}
+
 	session := dbclient.CreateSession()
 	defer dbclient.KillSession(session)
 
@@ -555,20 +625,14 @@ func UpdatePermissions(c *gin.Context) {
 	data, _ := ioutil.ReadAll(c.Request.Body)
 	if e := json.Unmarshal(data, &jsonData); e != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   e.Error(),
-			"isEmpty": true,
-			"data":    nil,
+			"error": e.Error(),
+			"data":  nil,
 		})
 		return
 	}
 
+	jsonData["myUID"] = uid
 	jsonData["uidB"] = c.Param("id")
-
-	var permissions int64 = 0
-	for i := 0; i < len(jsonData["permissions"].([]interface{})); i++ {
-		permissions |= jsonData["permissions"].([]interface{})[i].(int64) << (11 - i)
-	}
-	jsonData["permissions"] = permissions
 
 	_, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
 		_, err := transaction.Run(
@@ -586,13 +650,15 @@ func UpdatePermissions(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"error":   err.Error(),
-			"data":    nil,
+			"error": err.Error(),
+			"data":  nil,
 		})
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"error":   nil,
-		"data":    "Link Updated",
+		"error": nil,
+		"data": gin.H{
+			"code": jsonData["permissions"],
+		},
 	})
 }
