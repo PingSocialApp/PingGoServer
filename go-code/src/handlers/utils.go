cbbd1f64d3cdac590b1229950ef7d8e95bbcd6bc
Rajatkamal Polisety <30967352+Rajatkamal@users.noreply.github.com>
2021-07-12 22:04:15 -0500

    Migrated json data to struct
    All structs in Links handler now handle json data properly.

---
+++ go-code/src/handlers/utils.go
@@ -0,0 +1,56 @@
+package handlers
+
+import "reflect"
+
+func structToDbMap(item interface{}) map[string]interface{} {
+
+	res := map[string]interface{}{}
+	if item == nil {
+		return res
+	}
+	v := reflect.TypeOf(item)
+	reflectValue := reflect.ValueOf(item)
+	reflectValue = reflect.Indirect(reflectValue)
+
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	for i := 0; i < v.NumField(); i++ {
+		tag := v.Field(i).Tag.Get("db")
+		field := reflectValue.Field(i).Interface()
+		if tag != "" && tag != "-" {
+			if v.Field(i).Type.Kind() == reflect.Struct {
+				res[tag] = structToDbMap(field)
+			} else {
+				res[tag] = field
+			}
+		}
+	}
+	return res
+}
+func structToJsonMap(item interface{}) map[string]interface{} {
+
+	res := map[string]interface{}{}
+	if item == nil {
+		return res
+	}
+	v := reflect.TypeOf(item)
+	reflectValue := reflect.ValueOf(item)
+	reflectValue = reflect.Indirect(reflectValue)
+
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+	for i := 0; i < v.NumField(); i++ {
+		tag := v.Field(i).Tag.Get("jsonData")
+		field := reflectValue.Field(i).Interface()
+		if tag != "" && tag != "-" {
+			if v.Field(i).Type.Kind() == reflect.Struct {
+				res[tag] = structToJsonMap(field)
+			} else {
+				res[tag] = field
+			}
+		}
+	}
+	return res
+}
